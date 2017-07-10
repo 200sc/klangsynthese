f8153fe16efe5ae21e4f70c3b9110f25ba86b006
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-07-10 18:02:03 -0500

    Starting flac support, running into issues both in the flac decoding library (it doesn't support unencoded residuals) and in windows (0x80070057, or there is an invalid parameter to the dsbuff wave format)

---
+++ flac/flac_test.go
@@ -0,0 +1,25 @@
+package flac
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicFlac(t *testing.T) {
+	fmt.Println("Opening Basic Flac")
+	f, err := os.Open("test2.flac")
+	fmt.Println(f)
+	assert.Nil(t, err)
+	a, err2 := NewController().Load(f)
+	fmt.Println(a)
+	assert.Nil(t, err2)
+	fmt.Println("Now playing")
+	err = <-a.Play()
+	assert.Nil(t, err)
+	time.Sleep(8 * time.Second)
+	// In addition to the error tests here, this should play noise
+}
